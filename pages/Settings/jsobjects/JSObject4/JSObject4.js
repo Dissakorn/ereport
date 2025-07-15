56a4bfb942290d3d8752a4672abe275b0f4d86d8
dissakorn plubpla <dissakorn.p@hotmail.com>
2025-07-15 10:33:52 +0000

    huge update

--- pages/Settings/jsobjects/JSObject4/JSObject4.js
+++ pages/Settings/jsobjects/JSObject4/JSObject4.js
@@ -1,9 +1,21 @@
 export default {
-	Btn_Add_InvestigatoronClick () {
-		Select_public_investigator1.run(() => {
-			setTimeout(() => {
-				Inp_Inv_Name.setValue('');
-			}, 3000);
-		});
-	}
+  async Btn_Add_InvestigatoronClick () {
+    if (appsmith.store.isSubmitting) return;
+
+    storeValue("isSubmitting", true); // Lock button
+
+    try {
+      await Insert_public_investigator1.run(); // Insert
+      await Select_public_investigator2.run(); // Refresh
+
+      Inp_Inv_Name.setValue('');
+    } catch (e) {
+      showAlert("Something went wrong", "error");
+    }
+
+    // Wait 3 seconds before unlocking the button
+    setTimeout(() => {
+      storeValue("isSubmitting", false); // Unlock after 3 sec
+    }, 3000);
+  }
 }
